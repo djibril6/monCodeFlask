31d60f7a78250427ba6040712832a6db328410b3
Djib <issoufoudjib@gmail.com>
2019-04-10 12:10:02 +0100

    Création de la table user

--- app/models.py
+++ app/models.py
@@ -4,13 +4,21 @@ from .views import app
 
 db = SQLAlchemy(app)
 
-class Content(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(200), nullable=False)
-    gender = db.Column(db.Integer(), nullable=False)
+    nom = db.Column(db.String(60), nullable=False)
+    prenom = db.Column(db.String(50), nullable=False)
+    tel = db.Column(db.String(12), nullable=False)
+    email = db.Column(db.String(80), nullable=True)
+    date_inscription = db.Column(db.Date, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
-    def __init__(self, description, gender):
-        self.description = description
-        self.gender = gender
+    def __init__(self, nom, prenom, tel, email, compte, password):
+        self.nom = nom
+        self.prenom = prenom
+        self.tel = tel
+        self.email = email
+        self.compte = compte
+        self.password = password
 
 db.create_all()
--- app/views.py
+++ app/views.py
@@ -6,9 +6,12 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 @app.route('/')
-def index():
-	return render_template('index.html', content_type='application/json')
-    #return "Hello world !"
+def login():
+	return render_template('login.html', content_type='application/json')
+
+@app.route('/home/', methods=['POST', 'GET'])
+def home():
+	return render_template('home.html')
 
 if __name__ == "__main__":
     app.run()
